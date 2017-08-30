e343175040642b037d0033fccb1edb90846540d3
Aaron Bellis <aaron.bellis@utah.edu>
2017-08-30 13:38:31 -0600

    Make Hello World project

---
+++ Lab1/Hello1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello1
+{
+
+    class HelloWorld
+    {
+        /// <summary>
+        /// Writes "Hello World" to the console and waits for enter key to be pressed
+        /// to close the console window. 
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello World");
+            Console.In.ReadLine();
+        }
+    }
+
+}
