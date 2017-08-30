8150eb4a88948372036087fcfa4dc0e953258357
Aaron Bellis <aaron.bellis@utah.edu>
2017-08-30 14:09:23 -0600

    Set up solution for PS1

---
+++ PS1/FormulaEvaluator/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaEvaluator
+{
+    public static class Evaluator
+    {
+        public delegate int Lookup(String v);
+
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+            // TODO...
+        }
+    }
+}
