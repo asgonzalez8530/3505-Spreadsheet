247a4e92e5c523133c2336dcf480bbe99f90a499
Aaron Bellis <aaron.bellis@utah.edu>
2017-09-21 11:36:30 -0600

    Add name to top of test project.

--- PS3/FormulaClassTests/FormulaClassTests.cs
+++ PS3/FormulaClassTests/FormulaClassTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Tests written by Aaron Bellis u0981638 for PS3 solution in 
+// cs3500 fall 2017 semester
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FormulaClassTests
