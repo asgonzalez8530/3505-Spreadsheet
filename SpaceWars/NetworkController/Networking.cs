8d902406b72d6b00790d96e0e776f0a6936792ec
Aaron Bellis <aaron.bellis@utah.edu>
2017-11-09 12:24:29 -0700

    Add suggested method stubs for  network class

--- SpaceWars/NetworkController/Networking.cs
+++ SpaceWars/NetworkController/Networking.cs
@@ -1,6 +1,43 @@
-﻿namespace Communication
+﻿using System;
+using System.Net.Sockets;
+
+namespace Communication
 {
-    public class Class1
+    public class SocketState
     {
+
+    }
+
+    public static class Network
+    {
+        public static Socket ConnectToServer(Delegate callbackFunction, string hostname)
+        {
+            //TODO: Fix red lines (get delegate signature)
+        }
+
+        public static void ConnectedToServer(IAsyncResult stateAsArObject)
+        {
+            //TODO: Write method comment
+        }
+
+        public static void GetData(SocketState state)
+        {
+            //TODO: Write method comment
+        }
+
+        public static void ReceiveCallback(IAsyncResult stateAsArObject)
+        {
+           //TODO: write method comment
+        }
+
+        public static void Send(Socket socket, String data)
+        {
+            //TODO: write method comment
+        }
+
+        public static void SendCallback(IAsyncResult ar)
+        {
+            //TODO: write method comment
+        }
     }
 }
