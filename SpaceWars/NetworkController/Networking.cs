0a43cd9c15d10e227460847e5088d0fdeeabfeff
Aaron Bellis <aaron.bellis@utah.edu>
2017-11-09 12:28:31 -0700

    Add NotImplementedException to clear compile error

--- SpaceWars/NetworkController/Networking.cs
+++ SpaceWars/NetworkController/Networking.cs
@@ -13,31 +13,38 @@ namespace Communication
         public static Socket ConnectToServer(Delegate callbackFunction, string hostname)
         {
             //TODO: Fix red lines (get delegate signature)
+            throw new NotImplementedException();
         }
+
 
         public static void ConnectedToServer(IAsyncResult stateAsArObject)
         {
             //TODO: Write method comment
+            throw new NotImplementedException();
+
         }
 
         public static void GetData(SocketState state)
         {
             //TODO: Write method comment
+            throw new NotImplementedException();
         }
 
         public static void ReceiveCallback(IAsyncResult stateAsArObject)
         {
-           //TODO: write method comment
+            //TODO: write method comment
         }
 
         public static void Send(Socket socket, String data)
         {
             //TODO: write method comment
+            throw new NotImplementedException();
         }
 
         public static void SendCallback(IAsyncResult ar)
         {
             //TODO: write method comment
+            throw new NotImplementedException();
         }
     }
 }
