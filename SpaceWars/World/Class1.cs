e84c34fecfd530832301ab34996187f65c7997be
Aaron Bellis <aaron.bellis@utah.edu>
2017-11-13 00:34:30 -0700

    Create world project for model

---
+++ SpaceWars/World/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceWars
+{
+    public class World
+    {
+
+    }
+}
