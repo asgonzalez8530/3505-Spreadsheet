4b6446eb20e8c51fa089a0ddae3a7f79b6844895
Aaron Bellis <aaron.bellis@utah.edu>
2017-12-01 19:44:43 -0700

    Add server project to solution

---
+++ SpaceWars/Server/Program.cs
@@ -0,0 +1,18 @@
+﻿// Anastasia Gonzalez and Aaron Bellis UID: u0985898 & u0981638
+// Code implemented as part of PS8 : SpaceWars Server CS3500 Fall Semester
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceWarsServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
