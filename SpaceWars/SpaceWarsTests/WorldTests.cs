1a2f7998fac46ca3bf20af78976bbc365f26d4cc
Anastasia Gonzalez <gonzalez8530asg@gmail.com>
2017-12-09 14:44:18 -0700

    added more tests to the world

--- SpaceWars/SpaceWarsTests/WorldTests.cs
+++ SpaceWars/SpaceWarsTests/WorldTests.cs
@@ -8,6 +8,9 @@ namespace SpaceWarsTests
     [TestClass]
     public class WorldTests
     {
+
+        //************************ Valid Test ************************// 
+
         [TestMethod]
         public void SanityCheck()
         {
@@ -42,12 +45,218 @@ namespace SpaceWarsTests
         }
 
         [TestMethod]
-        public void TestWorld1()
+        public void TestWorldSize1()
         {
             // make a new world 
             World w = new World();
 
-            
+            // test default size
+            Assert.AreEqual(750, w.GetSize());
+
+            // change the universe size
+            w.SetUniverseSize("199");
+
+            // test that the default was changed
+            Assert.AreEqual(199, w.GetSize());
+        }
+
+        [TestMethod]
+        public void TestWorldSize2()
+        {
+            // make a new world 
+            World w = new World();
+
+            // change the universe size
+            w.SetUniverseSize(199);
+
+            // test that the default was changed
+            Assert.AreEqual(199, w.GetSize());
+        }
+
+        [TestMethod]
+        public void TestStartingHitPoints1()
+        {
+            // make a new world 
+            World w = new World();
+
+            // test default size
+            PrivateObject worldAccessor = new PrivateObject(w);
+            Assert.AreEqual(5, worldAccessor.GetField("startingHitPoints"));
+
+            // change the universe size
+            w.SetStartingHitPoint("199");
+
+            // test that the default was changed
+            Assert.AreEqual(199, worldAccessor.GetField("startingHitPoints"));
+        }
+
+        [TestMethod]
+        public void TestProjectileSpeed1()
+        {
+            // make a new world 
+            World w = new World();
+
+            // test default size
+            PrivateObject worldAccessor = new PrivateObject(w);
+            Assert.AreEqual(15, worldAccessor.GetField("projectileSpeed"));
+
+            // change the universe size
+            w.SetProjectileSpeed("199");
+
+            // test that the default was changed
+            Assert.AreEqual(199, worldAccessor.GetField("projectileSpeed"));
+        }
+
+        [TestMethod]
+        public void TestEngineStrength1()
+        {
+            // make a new world 
+            World w = new World();
+
+            // test default size
+            PrivateObject worldAccessor = new PrivateObject(w);
+            Assert.AreEqual(.08, worldAccessor.GetField("engineStrength"));
+
+            // change the universe size
+            w.SetEngineStrength("199");
+
+            // test that the default was changed
+            Assert.AreEqual((double) 199, worldAccessor.GetField("engineStrength"));
+        }
+
+        //************************ Invalid Test ************************// 
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestWorldSize3()
+        {
+            // make a new world 
+            World w = new World();
+
+            // try to set the size with null
+            w.SetUniverseSize("null");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestWorldSize4()
+        {
+            // make a new world 
+            World w = new World();
+
+            // try to set the size with an invalid number
+            w.SetUniverseSize("a199");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestWorldSize5()
+        {
+            // make a new world 
+            World w = new World();
+
+            // try to set the size with an invalid number
+            w.SetUniverseSize("Princess Aurora");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestStartingHitPoints2()
+        {
+            // make a new world 
+            World w = new World();
+
+            // try to set the hit points with null
+            w.SetStartingHitPoint("null");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestStartingHitPoints3()
+        {
+            // make a new world 
+            World w = new World();
+
+            // try to set the hit points with null
+            w.SetStartingHitPoint("a199");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestStartingHitPoints4()
+        {
+            // make a new world 
+            World w = new World();
+
+            // try to set the hit points with null
+            w.SetStartingHitPoint("Princess Jasmine");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestProjectileSpeed2()
+        {
+            // make a new world 
+            World w = new World();
+
+            // try to set the projectile speed with null
+            w.SetProjectileSpeed("null");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestProjectileSpeed3()
+        {
+            // make a new world 
+            World w = new World();
+
+            // try to set the projectile speed with null
+            w.SetProjectileSpeed("a199");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestProjectileSpeed4()
+        {
+            // make a new world 
+            World w = new World();
+
+            // try to set the projectile speed with null
+            w.SetProjectileSpeed("Princess Anna");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestEngineStrength2()
+        {
+            // make a new world 
+            World w = new World();
+
+            // try to set the projectile speed with null
+            w.SetEngineStrength("null");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestEngineStrength3()
+        {
+            // make a new world 
+            World w = new World();
+
+            // try to set the projectile speed with null
+            w.SetEngineStrength("a199");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestEngineStrength4()
+        {
+            // make a new world 
+            World w = new World();
+
+            // try to set the projectile speed with null
+            w.SetEngineStrength("Snow White");
         }
     }
 }
