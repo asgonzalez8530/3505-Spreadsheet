09eef5510c7439058133ad6a3c22ab84d82f61d7
Anastasia Gonzalez <gonzalez8530asg@gmail.com>
2017-12-13 18:51:47 -0700

    All thats left is parse the request for a name or game id and convert the table to html

--- SpaceWars/WebServer/WebServer.cs
+++ SpaceWars/WebServer/WebServer.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Communication;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace SpaceWarsServer
 {
@@ -14,6 +17,14 @@ namespace SpaceWarsServer
     {
         // The server must follow the HTTP protocol when replying to a client (web browser).
         // That involves sending an HTTP header before any web content.
+
+        /// <summary>
+        /// The connection string to our stats DB.
+        /// </summary>
+        public const string connectionString = "server=atr.eng.utah.edu;" +
+          "database=cs3500_u0981638;" +
+          "uid=cs3500_u0981638;" +
+          "password=AaronAndAnastasia";
 
         // This is the header to use when the server recognizes the browser's request
         private const string httpOkHeader =
@@ -65,12 +76,205 @@ namespace SpaceWarsServer
             Console.WriteLine("received http request: " + request);
 
 
-            // If the browser requested the home page (e.g., localhost/)
-            if (request.Contains("GET / HTTP/1.1"))
-                Network.SendAndClose(state.GetSocket(), httpOkHeader + "<h2>here is a web page!</h2>");
+            // If the browser requested the server stats
+            if (request.Contains("GET /scores HTTP/1.1"))
+            {
+                Network.SendAndClose(state.GetSocket(), httpOkHeader + ParseDataBaseForStats());
+            }
+
+            // If the browser requested a player
+            else if (Regex.IsMatch(request, @"GET /games?player=[\w\W]+ HTTP/ 1.1"))
+            {
+                // TODO: find a way to parse out the player name 
+                // maybe split?
+                string playerName = "";
+                string html = ParseDataBaseForPlayer(playerName);
+                if (html.Length == 0)
+                {
+                    Network.SendAndClose(state.GetSocket(), httpBadHeader + "<h2>the passed in player does not exist</h2>");
+
+                }
+                else
+                {
+                    Network.SendAndClose(state.GetSocket(), httpOkHeader + html);
+                }
+            }
+
+            // If the browser requested a game
+            else if (Regex.IsMatch(request, @"GET /game?id=\d+ HTTP/ 1.1"))
+            {
+                // TODO: find a better way because the number could be greater than ten
+                // see if the id is a valid number
+                if (int.TryParse(request.Substring(12, 1), out int gameID))
+                {
+                    string html = ParseDataBaseForGame(gameID);
+                    if (html.Length == 0)
+                    {
+                        Network.SendAndClose(state.GetSocket(), httpBadHeader + "<h2>the passed in game ID does not exist</h2>");
+                    }
+                    else
+                    {
+                        Network.SendAndClose(state.GetSocket(), httpOkHeader + html);
+                    }
+                }
+            }
+
             // Otherwise, our very simple web server doesn't recognize any other URL
             else
+            {
                 Network.SendAndClose(state.GetSocket(), httpBadHeader + "<h2>page not found</h2>");
+            }
+        }
+
+        private static string ParseDataBaseForStats()
+        {
+            // Connect to the DB
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    // Open a connection
+                    conn.Open();
+
+                    string html = "";
+                    // Create a command
+                    using (MySqlCommand command = conn.CreateCommand())
+                    {
+                        // This will be the command that we will execute
+                        string commandString = "select playerName, score, accuracy from PlayersInGame" +
+                                                "natural join PlayerStats;";
+
+                        // give our comand to the MySqlCommand object
+                        command.CommandText = commandString;
+
+                        // execute the command
+                        command.ExecuteNonQuery();
+
+                        // Execute the command
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                //TODO: make a table in html
+                                string playerName = reader["playerName"].ToString();
+                                string score = reader["score"].ToString();
+                                string accuracy = reader["accuracy"].ToString();
+                            }
+                        }
+                    }
+
+                    // close our connection
+                    conn.Close();
+                    return html;
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message + " : Stats");
+                    return "";
+                }
+            }
+        }
+
+        private static string ParseDataBaseForPlayer(string playerName)
+        {
+            // Connect to the DB
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    // Open a connection
+                    conn.Open();
+
+                    string html = "";
+                    // Create a command
+                    using (MySqlCommand command = conn.CreateCommand())
+                    {
+                        // This will be the command that we will execute
+                        string commandString = "select playerName, score, accuracy from PlayersInGame" +
+                                                "natural join PlayerStats where gameID=" + playerName + ";";
+
+                        // give our comand to the MySqlCommand object
+                        command.CommandText = commandString;
+
+                        // execute the command
+                        command.ExecuteNonQuery();
+
+                        // Execute the command
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                //TODO: make a table in html
+                                //string playerName = reader["playerName"].ToString();
+                                string score = reader["score"].ToString();
+                                string accuracy = reader["accuracy"].ToString();
+                            }
+                        }
+                    }
+
+                    // close our connection
+                    conn.Close();
+                    return html;
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message + " : Stats");
+                    return "";
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private static string ParseDataBaseForGame(int gameID)
+        {
+            // Connect to the DB
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    // Open a connection
+                    conn.Open();
+
+                    string html = "";
+                    // Create a command
+                    using (MySqlCommand command = conn.CreateCommand())
+                    {
+                        // This will be the command that we will execute
+                        string commandString = "select playerName, score, accuracy from PlayersInGame" +
+                                                "natural join PlayerStats where gameID=" + gameID + ";";
+
+                        // give our comand to the MySqlCommand object
+                        command.CommandText = commandString;
+
+                        // execute the command
+                        command.ExecuteNonQuery();
+
+                        // Execute the command
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                //TODO: make a table in html
+                                string playerName = reader["playerName"].ToString();
+                                string score = reader["score"].ToString();
+                                string accuracy = reader["accuracy"].ToString();
+                            }
+                        }
+                    }
+
+                    // close our connection
+                    conn.Close();
+                    return html;
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message + " : Game");
+                    return "";
+                }
+            }
         }
 
         // NOTE: The above SendAndClose calls are an addition to the Networking library.
