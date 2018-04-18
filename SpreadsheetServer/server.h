6ddd22112be35ee9a059d93519546d27eee1a33a
Anastasia Gonzalez <Anastasia@anastasias-mbp.uconnect.utah.edu>
2018-04-18 11:11:23 -0600

    combine mike and my code

--- SpreadsheetServer/server.h
+++ SpreadsheetServer/server.h
@@ -45,8 +45,8 @@ namespace cs3505
             void check_for_shutdown();
             void shutdown();
             std::string parseBuffer(std::string * message);
-            void parse_and__message(std::string message);
-			std::string parse_message(std::string message);
+            //void parse_and__message(std::string message);
+			std::string parse_message(spreadsheet, std::string message);
 
 
     };
